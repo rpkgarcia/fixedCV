dd64416a3782b1e07b150abff87d7a1bed9ab302
Thomas Robacker <trobacker21@gmail.com>
2025-06-26 16:34:04 -0400

    Streamlined simulation script.

---
+++ sims/univariate_simulations.R
@@ -0,0 +1,49 @@
+# Univariate simulations
+source("sims/helper-functions.R")
+
+seed.value = 1234
+set.seed(seed.value)
+nsim <- 100    # Number of simulations for Type errors
+alpha <- 0.05   # Significance level
+big_T <- 200   # Time Series length
+d = 1           # X dimension (univariate Y for now)
+
+# Example data set
+# data_homo <- AR1_AR_u(big_T = big_T, rho = rho, d = d, theta = rep(0,d))
+# y <- data_homo$Y
+# x <- data_homo$X
+# big_T <- nrow(x)
+#
+# plot(y~c(1:big_T), type = "l")
+
+# Select kernel(s)
+# the_kernel <- bartlett
+# the_kernel <- parzen
+# the_kernel <- qs
+# the_kernel <- th
+
+# Parzen, Bartlett, QS, TH - string for printing
+# the_kernel_string <- 'Bartlett' #main focus
+# the_kernel_string <- 'Parzen'
+# the_kernel_string <- 'QS'
+# the_kernel_string <- 'TH'
+
+# Lugsail setting
+# lugsail = "Mother"
+# lugsail = "Zero"
+# lugsail = "Over"
+
+# Method
+# method = "simulated"
+#method = "analytical"
+
+# tau parameter, use default for now
+# tau = 0.05*.15
+
+sim1 <- simulate_t1error_rate_single_rho(nsim = 1000, data_generation_fcn = AR1_AR_u, big_T = big_T, d = d, theta = rep(0,d))
+sim2 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 1235, data_generation_fcn = AR1_SINE, big_T = big_T, d = d, theta = rep(0,d))
+
+d = 2
+sim3 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 1236, data_generation_fcn = AR1_AR_u, big_T = big_T, d = d, theta = rep(0,d))
+sim4 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 1237, data_generation_fcn = AR1_SINE, big_T = big_T, d = d, theta = rep(0,d))
+

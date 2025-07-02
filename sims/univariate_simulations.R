83e2c431a44ed2917707892fd27a064254f619b6
Thomas Robacker <trobacker21@gmail.com>
2025-07-02 09:52:43 -0400

    Simplify conventions and add file writes

--- sims/univariate_simulations.R
+++ sims/univariate_simulations.R
@@ -22,22 +22,37 @@ d = 1           # X dimension (univariate Y for now)
 # fitr <- robust_lm(fit = fit)
 
 
-sim1 <- simulate_t1error_rate_single_rho(nsim = 1000, data_generation_fcn = AR1_AR_u, big_T = big_T, d = d, theta = rep(0,d))
-sim2 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 1235, data_generation_fcn = AR1_SINE, big_T = big_T, d = d, theta = rep(0,d))
+sim1 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 123, data_generation_fcn = AR1_AR_u, big_T = big_T, d = d, theta = rep(0,d))
+sim2 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 124, data_generation_fcn = AR1_SINE, big_T = big_T, d = d, theta = rep(0,d))
 sim_ar1_het <-
-  simulate_t1error_rate_single_rho(nsim = 1000, data_generation_fcn = AR1_HET, big_T = big_T, d = d, theta = rep(0,d))
+  simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 125, data_generation_fcn = AR1_HET, big_T = big_T, d = d, theta = rep(0,d))
+
+write.csv(x = sim1, file = "./sims/d=1/ar1_ar_u.csv")
+write.csv(x = sim2, file = "./sims/d=1/ar1_sine.csv")
+write.csv(x = sim_ar1_het, file = "./sims/d=1/ar1_het.csv")
+beep("complete")
 
 d = 2
-sim1 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 1236, data_generation_fcn = AR1_AR_u, big_T = big_T, d = d, theta = rep(0,d))
-sim2 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 1237, data_generation_fcn = AR1_SINE, big_T = big_T, d = d, theta = rep(0,d))
+sim1 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 126, data_generation_fcn = AR1_AR_u, big_T = big_T, d = d, theta = rep(0,d))
+sim2 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 127, data_generation_fcn = AR1_SINE, big_T = big_T, d = d, theta = rep(0,d))
 sim_ar1_het <-
-  simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 1235, data_generation_fcn = AR1_HET, big_T = big_T, d = d, theta = rep(0,d))
+  simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 128, data_generation_fcn = AR1_HET, big_T = big_T, d = d, theta = rep(0,d))
+
+write.csv(x = sim1, file = "./sims/d=2/ar1_ar_u.csv")
+write.csv(x = sim2, file = "./sims/d=2/ar1_sine.csv")
+write.csv(x = sim_ar1_het, file = "./sims/d=2/ar1_het.csv")
+beep("complete")
 
 d = 3
-sim1 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 31, data_generation_fcn = AR1_AR_u, big_T = big_T, d = d, theta = rep(0,d))
-sim2 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 32, data_generation_fcn = AR1_SINE, big_T = big_T, d = d, theta = rep(0,d))
+sim1 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 129, data_generation_fcn = AR1_AR_u, big_T = big_T, d = d, theta = rep(0,d))
+sim2 <- simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 130, data_generation_fcn = AR1_SINE, big_T = big_T, d = d, theta = rep(0,d))
 sim_ar1_het <-
-  simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 33, data_generation_fcn = AR1_HET, big_T = big_T, d = d, theta = rep(0,d))
+  simulate_t1error_rate_single_rho(nsim = 1000, seed.value = 131, data_generation_fcn = AR1_HET, big_T = big_T, d = d, theta = rep(0,d))
+write.csv(x = sim1, file = "./sims/d=3/ar1_ar_u.csv")
+write.csv(x = sim2, file = "./sims/d=3/ar1_sine.csv")
+write.csv(x = sim_ar1_het, file = "./sims/d=3/ar1_het.csv")
+beep("complete")
 
+beep("complete")
 # Save example
 # write.csv(x = sim_ar1_het_1, file = "./sims/d=1/ar1_het.csv")

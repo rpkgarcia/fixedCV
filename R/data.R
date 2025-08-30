fffd3e4f581d4628c1865777492c03cc6770a20f
Rebecca Kurtz-Garcia <REBECCAKURTZ62@GMAIL.COM>
2025-08-30 19:30:31 -0400

    Update data.R

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A subset of data from the World Health Organization Global Tuberculosis
 #' Report ...
 #'
-#' @format ## `who`
+#' @format ## `fitted_CV`
 #' A data frame with 7,240 rows and 60 columns:
 #' \describe{
 #'   \item{country}{Country name}
